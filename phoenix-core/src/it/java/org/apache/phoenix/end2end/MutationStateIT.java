8dcd24d6b72fa93c6346fd457059b6b324e2316b
Neha <neha.gupta@salesforce.com>
2020-01-08 13:28:20 -0800

    PHOENIX: 5630 MAX_MUTATION_SIZE_EXCEEDED and MAX_MUTATION_SIZE_BYTES_EXCEEDED SQLExceptions should print existing size
    Signed-off-by: Chinmay Kulkarni <chinmayskulkarni@apache.org>

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/MutationStateIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/MutationStateIT.java
@@ -364,6 +364,8 @@ public class MutationStateIT extends ParallelStatsDisabledIT {
                 e.getErrorCode());
             assertTrue(e.getMessage().contains(
                     SQLExceptionCode.MAX_MUTATION_SIZE_EXCEEDED.getMessage()));
+            assertTrue(e.getMessage().contains(
+                    connectionProperties.getProperty(QueryServices.MAX_MUTATION_SIZE_ATTRIB)));
         }
 
         // set the max mutation size (bytes) to a low value
@@ -379,6 +381,8 @@ public class MutationStateIT extends ParallelStatsDisabledIT {
                 e.getErrorCode());
             assertTrue(e.getMessage().contains(
                     SQLExceptionCode.MAX_MUTATION_SIZE_BYTES_EXCEEDED.getMessage()));
+            assertTrue(e.getMessage().contains(connectionProperties.getProperty
+                    (QueryServices.MAX_MUTATION_SIZE_BYTES_ATTRIB)));
         }
     }
 
